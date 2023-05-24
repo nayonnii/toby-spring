4ce40aafb3b5ac7dbc6a265143838c2bd1053ef8
nayonnii <nyovv26@naver.com>
2023-05-24 14:35:47 +0900

    feat: SimpleConnectionMaker로 분리 fix: UserDao수정 - DUserDao, NUserDao 생성에 따른 수정

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -7,25 +7,11 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public abstract class UserDao {
-
-    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-
-//    public Connection getConnection() throws ClassNotFoundException, SQLException {
-//        Map<String, String> env = getenv();
-//        String dbHost = env.get("DB_HOST");
-//        String dbUser = env.get("DB_USER");
-//        String dbPassword = env.get("DB_PASSWORD");
-//
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
-//
-//        return conn;
-//    }
-
+public class UserDao {
+    SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-          Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values (?, ? ,?)");
         pstmt.setString(1, user.getId());
@@ -39,7 +25,7 @@ public abstract class UserDao {
 
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-          Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("select id, name, password from users where id = ?");    // 모든 걸 호출하더라도 컬럼명을 써주는 게 좋음
         pstmt.setString(1, id);
@@ -60,14 +46,14 @@ public abstract class UserDao {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new NUserDao();
+        UserDao userDao = new UserDao();
         User user = new User();
-        user.setId("2");
-        user.setName("jihoon");
-        user.setPassword("123456");
+        user.setId("3");
+        user.setName("jinyoung");
+        user.setPassword("1234");
         userDao.add(user);
 
-        User selectedUser = userDao.get("2");
+        User selectedUser = userDao.get("3");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
