5a2a5b23a8c8e11729b2c72ea57e8feb7c5b9ace
nayonnii <nyovv26@naver.com>
2023-05-24 15:39:58 +0900

    feat: UserDaoTest 분리

---
+++ src/main/java/com/example/tobyspring3/dao/UserDaoTest.java
@@ -0,0 +1,24 @@
+package com.example.tobyspring3.dao;
+
+import com.example.tobyspring3.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+
+        User user = new User();
+        user.setId("6");
+        user.setName("kwanghyun");
+        user.setPassword("12345678");
+        userDao.add(user);
+
+        User selectedUser = userDao.get("6");
+        System.out.println(selectedUser.getId());
+        System.out.println(selectedUser.getName());
+        System.out.println(selectedUser.getPassword());
+
+    }
+}
